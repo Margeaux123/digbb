87999d38c8a39e50861a146bdcd8c77e0ebffc3e
Margeaux Mann <manndesign@aol.com>
2013-07-23 16:28:41 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Digbb::Application.config.secret_token = 'ebbeb0dbf56f7a0c92f9b76ec016d3ea4c6de409e855493dfc74c089faa58c8e397d847987380ba13709c79a955b7aa069647dbd260b893f73dd8cb5a7c70f21'
